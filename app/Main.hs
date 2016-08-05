f5511e5aa4b5b800288ff2a88e58fa2d268d8b3c
Michael Fowler <michael@Michaels-MacBook-Pro.local>
2016-08-05 15:36:36 +1200

    Stub Solver module to unfurl silhouettes

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import           Lib
+import           Solver
 import           System.Exit (die)
 
 main :: IO ()
---
+++ src/Solver.hs
@@ -0,0 +1,10 @@
+module Solver
+where
+
+import Lib
+import Data.Graph.AStar
+import Data.Set
+
+unfurl :: Silhouette -> Set Silhouette
+unfurl _ = undefined
+
