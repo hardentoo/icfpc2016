970b6ae2fce0fe80ca81a866532c30933fdc8a57
Michael Fowler <michael@Michaels-MacBook-Pro.local>
2016-08-05 15:58:17 +1200

    Outline a prospective implementaiton for unfurl

--- src/Solver.hs
+++ src/Solver.hs
@@ -3,8 +3,20 @@ where
 
 import Lib
 import Data.Graph.AStar
-import Data.Set
+import Data.Set (Set)
 
 unfurl :: Silhouette -> Set Silhouette
 unfurl _ = undefined
-
+{-
+  for each polygon in the silhouette:
+    for each edge in the polygon:
+      each of:
+        | replicate polygon, reflected across the edge
+        +--| leave skeleton intact
+        |  | trim skeleton
+        | *move* polygon, reflected across the edge
+        +--| leave skeleton intact
+        |  | trim skeleton
+        = filter:
+          in some sense coherent (e.g. total area, adjacency...)
+-}
