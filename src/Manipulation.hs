d9a6f786e494c1351dddd4f014b62a999591dddb
Michael Fowler <mkrfowler@gmail.com>
2016-08-05 17:38:38 +1200

    Add tentative background support for reflection about an edge

--- src/Manipulation.hs
+++ src/Manipulation.hs
@@ -8,11 +8,25 @@ import Problem
 import Data.Matrix
 
 pointsToMatrix :: [Point] -> Matrix Rational
-pointsToMatrix points = fromLists lists where
-  destructure (Point (Coord x) (Coord y)) = [x,y]
-  lists = map destructure points
+pointsToMatrix points = fromLists (map destructure points)
 
 matrixToPoints :: Matrix Rational -> [Point]
-matrixToPoints matrix = map restructure lists where
-  restructure [x,y] = Point (Coord x) (Coord y)
-  lists = toLists matrix
+matrixToPoints matrix = map restructure (toLists matrix)
+
+destructure :: Point -> [Rational]
+destructure (Point (Coord x) (Coord y)) = [x,y]
+
+restructure :: [Rational] -> Point
+restructure [x,y] = Point (Coord x) (Coord y)
+
+
+mirrorPoints :: [Point] -> Segment -> Point
+mirrorPoints _ _ = undefined
+
+
+mirrorMatrix :: Segment -> Matrix Rational
+mirrorMatrix (Segment p p') = scaleMatrix normaliser transform where
+  Point (Coord x) (Coord y)   = p
+  Point (Coord x') (Coord y') = p'
+  normaliser = 1 / ((x' - x)^2 + (y' - y)^2)
+  transform = fromLists [ [x^2 - y^2, 2 * x * y], [2 * x * y, y^2 - x^2] ]
