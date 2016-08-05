b8f690c0134631fd435218597ebcdd24fd66463b
Ben Anderson <benanerson@acidic.co.nz>
2016-08-06 10:52:12 +1200

    Fixed viewer to cope with differing scales and reference frames

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -13,17 +13,17 @@ window.addEventListener("DOMContentLoaded", function() {
   const renderer  = new Renderer(canvas);
 
   const renderUI = function() {
-    renderer.render(window.problemSet[window.currentProblem]);
+    renderer.render(window.problemSet.problems[window.currentProblem], window.problemSet, true);
 
-    maxProblems.innerText = window.problemSet.length;
+    maxProblems.innerText = window.problemSet.problems.length;
     currentProblem.value = window.currentProblem + 1;
   }
 
   const renderPreviews = function() { 
     previews.innerHTML = "";
 
-    for(let i = 0; i < window.problemSet.length; i++) {
-      let problem = window.problemSet[i];
+    for(let i = 0; i < window.problemSet.problems.length; i++) {
+      let problem = window.problemSet.problems[i];
       let canvas = document.createElement("canvas");
       canvas.classList.add("preview-canvas");
       canvas.width  = "100";
@@ -35,7 +35,7 @@ window.addEventListener("DOMContentLoaded", function() {
       previews.appendChild(canvas);
 
       let previewRenderer = new Renderer(canvas);
-      previewRenderer.render(problem);
+      previewRenderer.render(problem, window.problemSet);
     }
   }
 
@@ -93,15 +93,39 @@ class Problem {
   }
 }
 
+class ProblemSet {
+  constructor(pbs, cx0, cy0, cx1, cy1) {
+    console.log("new ProblemSet(): ", pbs, cx0, cy0, cx1, cy1);
+    this.problems = pbs;
+    this.x0 = cx0;
+    this.y0 = cy0;
+    this.x1 = cx1;
+    this.y1 = cy1;
+  }
+}
+
 class Renderer {
   constructor(cvs) {
     this.canvas   = cvs;
     this.context  = cvs.getContext("2d");
   }
   
-  render(problem) {
-    let width = this.canvas.width;
-    let height = this.canvas.height;
+  render(problem, set, renderLabels) {
+    let width = this.canvas.width * 0.8;
+    let height = this.canvas.height * 0.8;
+    let left = this.canvas.height * 0.1;
+    let top = this.canvas.height * 0.1;
+
+    let scaleX = 1 / (set.x1 - set.x0);
+    let scaleY = 1 / (set.y1 - set.y0);
+
+    console.log("scaleX:", scaleX);
+    console.log("scaleY:", scaleY);
+    console.log("x0:", set.x0);
+    console.log("y0:", set.y0);
+    console.log("x1:", set.x1);
+    console.log("y1:", set.y1);
+    console.log("set:", set);
 
     this.context.clearRect(0, 0, width, height);
     for(let polygon of problem.polygons) {
@@ -111,10 +135,10 @@ class Renderer {
 
       for(let point of polygon.points) {
         if(first) {
-          this.context.moveTo(point.x * width, height - (point.y * height));
+          this.context.moveTo(left + ((point.x - set.x0) * width * scaleX), top + (height - ((point.y - set.y0) * height * scaleY)));
           first = false;
         } else {
-          this.context.lineTo(point.x * width, height - (point.y * height));
+          this.context.lineTo(left + ((point.x - set.x0) * width * scaleX), top + (height - ((point.y - set.y0) * height * scaleY)));
         }
       }
 
@@ -127,16 +151,38 @@ class Renderer {
 
 
       for(let bone of polygon.bones) {
-        this.context.moveTo(bone.p0.x * width, height - (bone.p0.y * height));
-        this.context.lineTo(bone.p1.x * width, height - (bone.p1.y * height));
+        this.context.moveTo(left + ((bone.p0.x - set.x0) * width * scaleX), top + (height - ((bone.p0.y - set.y0) * height * scaleY)));
+        this.context.lineTo(left + ((bone.p1.x - set.x0) * width * scaleX), top + (height - ((bone.p1.y - set.y0) * height * scaleY)));
       }
 
-      this.context.strokeStyle = "rgb(0, 00, 0)";
+      this.context.strokeStyle = "rgb(0, 0, 0)";
       this.context.lineWidth = 2;
 
       this.context.stroke();
+
+      this.context.fillStyle = "rgb(0, 0, 0)";
+      this.context.font = "14px sans-serif";
+
+      if(renderLabels) {
+        this.context.fillText(set.x0, left + 4, top + height + 16, 20);
+        this.context.fillText(set.x1, left + width, top + height + 16, 20);
+        this.context.fillText(set.y0, left - 25, top + height, 20);
+        this.context.fillText(set.y1, left - 25, top + 12, 20);
+      }
     }
   }
+}
+
+function nestedBest(problems, start, axis, comparator) {
+  return problems.reduce(function(b1, problem) {
+    let sub = problem.polygons.reduce(function(b2, polygon) {
+      let subsub = polygon.points.reduce(function(b3, point) {
+        return (comparator(point[axis], b3) ? point[axis] : b3);
+      }, start);
+      return (comparator(subsub, b2) ? subsub : b2);
+    }, start);
+    return (comparator(sub, b1) ? sub : b1);
+  }, start);
 }
 
 class Parser {
@@ -146,7 +192,6 @@ class Parser {
 
     while(lines.length > 0) {
       let polygons = [];
-      
 
       const numberOfPolygons = lines.shift();
       
@@ -157,7 +202,12 @@ class Parser {
       problems.push(new Problem(polygons));
     }
 
-    return problems;
+    return new ProblemSet(problems,
+      nestedBest(problems, 1e+1000000,  "x", (a, b) => a < b),
+      nestedBest(problems, 1e+10000,  "y", (a, b) => a < b),
+      nestedBest(problems, 1e-10000000, "x", (a, b) => a > b),
+      nestedBest(problems, 1e-10000, "y", (a, b) => a > b)
+    );
   }
 
   static parsePolygon(lines) {
